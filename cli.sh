225cc45e523c5f1fbe6cbdeb934e27806e73599a
LuisEGR <luisegr149@gmail.com>
2022-07-05 13:45:10 -0500

    Test env variables and secrets

---
+++ cli.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+function replaceEnv {
+    set -a
+    . test.env
+    set +a
+}
+
+function fillTemplate {
+    replaceEnv
+    cat template.txt | envsubst > output.txt
+    cat output.txt
+}
+
+fillTemplate
